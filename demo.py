96fd7cb57dcd6e99ee50e937baa041318a9626e7
lizheng9393 <1627788642@qq.com>
2021-05-21 23:26:14 +0800

    第一次操作这么久

--- demo.py
+++ demo.py
@@ -1,2 +1,25 @@
+"""
 print("hello world")
 print("你好，这是我第一次操作python!")
+print("xixi","haha ",2333)
+
+
+
+# print("嘻嘻"+"哈哈")
+# name="张三"
+# print(name)
+# a=input("请输入：")
+# print("这是我刚刚输入的值",a)
+# """
+# a=input("请输入第一个数字：")
+# b=input("请输入第二个数字：")
+# print("他们的和是",float(a)+float(b))
+
+
+# a=len("哈哈哈哈哈哈哈哈哈哈哈哈")
+# print(a)
+print("o代表偶数""1代表奇数")
+lstr=input("请输入你想要输入的内容")
+print(len(lstr)%2)
+
+
